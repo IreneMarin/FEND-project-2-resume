a04c3a50375b007e30ead27fc97000529c0de64a
Irene Marin <irene@marin.cat>
2015-08-03 21:21:26 +0200

    New project

---
+++ js/helper.js
@@ -0,0 +1,220 @@
+﻿/*
+These are HTML strings. As part of the course, you'll be using JavaScript functions
+replace the %data% placeholder text you see in them.
+*/
+var HTMLheaderName = '<h1 id="name">%data%</h1><hr/>';
+
+// Since I am doing the "resume" like a kind of biography of Da Vinci, I changed the contacts variables:
+var HTMLheaderRole = '<li class="flex-item"><span class="orange-text">%data%</span></li>';
+var HTMLbirthdate = '<li class="flex-item"><span class="orange-text">Born</span><span class="white-text">%data%</span></li>';
+var HTMLbirthplace = '<li class="flex-item"><span class="orange-text">At</span><span class="white-text">%data%</span></li>';
+var HTMLdeaddate = '<li class="flex-item"><span class="orange-text">Died</span><span class="white-text">%data%</span></li>';
+var HTMLdeadplace = '<li class="flex-item"><span class="orange-text">At</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic" alt="%alt%">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">The genius at a glance:</h3><ul id="skills" class="flex-box skills"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%&nbsp;';
+var HTMLworkTitle = '  ܀܀ &nbsp;%data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%&nbsp;&nbsp;</div>';
+var HTMLworkLocation = '<div class="location-text">⚜&nbsp;&nbsp;%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%" alt="%alt%">';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = '&nbsp;܀܀ %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%&nbsp;&nbsp;</div>';
+var HTMLschoolLocation = '<div class="location-text">⚜&nbsp;&nbsp;%data%</div>';
+var HTMLschoolMajor = '<p><br>%data%</p>';
+
+var googleMap = '<div id="map"></div>';
+
+/*
+The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+Part 5 for project 2.
+*/
+clickLocations = [];
+function logClicks(x, y) {
+    clickLocations.push(
+      {
+          x: x,
+          y: y
+      }
+    );
+    console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function (loc) {
+    console.log(loc.pageX, loc.pageY);
+});
+
+// MAPS!
+var map;    // declares a global map variable
+
+// Start here! initializeMap() is called when page is loaded.
+
+function initializeMap() {
+
+    var locations;
+    var descriptions;
+    var mapOptions = {
+        disableDefaultUI: true
+    };
+
+    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+    // locationFinder() returns an array of every location string from the JSONs written for bio, education, and work.
+
+    function locationFinder() {
+
+        // initializes an empty array
+        var locations = [];
+
+        // adds the location of birth and death:
+        locations.push(bio.birth.location);
+        locations.push(bio.death.location);
+
+        // iterates through school locations and appends each location to the locations array
+        var lengthSchools = education.schools.length;
+        for (var school = 0; school < lengthSchools; school++) {
+            locations.push(education.schools[school].location);
+        }
+
+        // iterates through work locations and appends each location to the locations array
+        var lengthJobs = work.jobs.length;
+        for (var job = 0; job < lengthJobs; job++) {
+            locations.push(work.jobs[job].location);
+        }
+
+        return locations;
+    }
+
+
+    function descriptionFinder(location) {
+
+        var description;
+
+        // iterates through school locations and appends each location to the locations array
+        var lengthSchools = education.schools.length;
+        for (var school = 0; school < lengthSchools; school++) {
+            if (location === education.schools[school].location) {
+                description = education.schools[school].description;
+            }
+        }
+
+        // iterates through work locations and appends each location to the locations array
+        var lengthJobs = work.jobs.length;
+        for (var job = 0; job < lengthJobs; job++) {
+            if (location === work.jobs[job].location) {
+                description = work.jobs[job].description;
+            }
+        }
+
+        return description;
+    }
+
+    /*
+    createMapMarker(placeData) reads Google Places search results to create map pins.
+    placeData is the object returned from search results containing information about a single location.
+    */
+    function createMapMarker(placeData) {
+
+        // The next lines save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat();  // latitude from the place service
+        var lon = placeData.geometry.location.lng();  // longitude from the place service
+        var name = placeData.name;   // name of the place from the place service
+        var description = descriptionFinder(name);
+        var bounds = window.mapBounds;            // current boundaries of the map window
+
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeData.geometry.location,
+            // changing the original icon map
+            icon: './images/mapa.png'
+        });
+        
+        // the content inside the windows that opens when we click the markers in the map
+        var contentString = '<div id="content"><div id="siteNotice"></div>' +
+        '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' +        
+        '</div>';
+
+        // infoWindows are the little helper windows that open when you click or hover over a pin on a map.
+        // They usually contain more information about a location.
+        var infoWindow = new google.maps.InfoWindow({
+            // Here to put the info that will show up in the windows
+            content: contentString
+        });
+
+        // hmmmm, I wonder what this is about...
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(map, marker);
+        });
+
+        // this is where the pin actually gets added to the map.
+        // bounds.extend() takes in a map location object
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        // fit the map to the new marker
+        map.fitBounds(bounds);
+        // center the map
+        map.setCenter(bounds.getCenter());
+    }
+
+    /*
+    callback(results, status) makes sure the search returned results for a location.
+    If so, it creates a new map marker for that location.
+    */
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            createMapMarker(results[0]);
+        }
+    }
+
+    // pinPoster(locations) takes in the array of locations created by locationFinder() and fires off Google place searches for each location
+    function pinPoster(locations) {
+
+        // creates a Google place search service object. PlacesService does the work of actually searching for location data.
+        var service = new google.maps.places.PlacesService(map);
+
+        // Iterates through the array of locations, creates a search object for each location
+        var lengthLocations = locations.length;
+        for (var place = 0; place < lengthLocations; place++) {
+
+            // the search request object
+            var request = {
+                query: locations[place]
+            };
+
+            // Actually searches the Google Maps API for location data and runs the callback function with the search results after each search.
+            service.textSearch(request, callback);
+        }
+    }
+
+    // Sets the boundaries of the map based on pin locations
+    window.mapBounds = new google.maps.LatLngBounds();
+
+    // locations is an array of location strings returned from locationFinder()
+    locations = locationFinder();
+
+    // pinPoster(locations) creates pins on the map for each location in the locations array
+    pinPoster(locations);
+}
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window and adjust map bounds
+window.addEventListener('resize', function (e) {
+    //Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,319 @@
+﻿// Create the object 'bio'
+var bio = {
+    'name': 'Leonardo di ser Piero da Vinci',
+    'role': 'Italian Renaissence polymath',
+    'welcomeMessage': '❝ In the normal course of events many men and women are born with remarkable talents; but occasionally, in a way that transcends nature, a single person is marvellously endowed by Heaven with beauty, grace and talent in such abundance that he leaves other men far behind, all his actions seem inspired and indeed everything he does clearly comes from God rather than from human skill. Everyone acknowledged that this was true of Leonardo da Vinci, an artist of outstanding physical beauty, who displayed infinite grace in everything that he did and who cultivated his genius so brilliantly that all problems he studied he solved with ease.❞ — Giorgio Vasari',
+
+    // I changed the variables of the 'contacts' part (mobile, twitter, etc) to make it more viable for a Renaissance biography
+    'birth': {
+        'date': '15 April 1452',
+        'location': 'Vinci, Florence, Italy'
+    },
+    'death': {
+        'date': '2 May 1519',
+        'location': 'Amboise, France'
+    },
+
+    'skills': ['painter', 'sculptor', 'architect', 'musician', 'mathematician', 'engineer', 'inventor', 'anatomist', 'geologist', 'astronomer', 'cartographer', 'botanist', 'historian', 'writer'],
+    'bioPic': 'images/leonardo.jpg'
+};
+
+// Display the function with the bio info
+function displayBio() {
+
+    // Prepend the name in the website
+    $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
+
+    // Append the role, birth and death information at the header of the website
+    $('#topContacts').append(HTMLheaderRole.replace('%data%', bio.role));
+    $('#topContacts').append(HTMLbirthdate.replace('%data%', bio.birth.date));
+    $('#topContacts').append(HTMLbirthplace.replace('%data%', bio.birth.location));
+    $('#topContacts').append(HTMLdeaddate.replace('%data%', bio.death.date));
+    $('#topContacts').append(HTMLdeadplace.replace('%data%', bio.death.location));
+
+    // Append the image and the welcome message
+    $('#header').append(HTMLbioPic.replace('%data%', bio.bioPic).replace('%alt%', bio.name));
+    $('#header').append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
+
+    if (bio.skills.length > 0) {
+        var len = bio.skills.length;
+        // Append all the skills, and the skills title
+        $('#header').append(HTMLskillsStart);
+        for (var skill = 0; skill < len; skill++) {
+            $('#skills').append(HTMLskills.replace('%data%', '܀܀ ' + bio.skills[skill]));
+        }
+    }
+}
+displayBio();
+
+// Create the object 'education'
+var education = {
+    'schools': [
+        {
+            'name': 'Andrea del Verrocchio workshop',
+            'location': 'Florence, Italy',
+            'degree': 'Apprentice',
+            'majors': ['Master in the Guild of Saint Luke'],
+            'dates': '1466 - 1476',
+            'url': 'https://en.wikipedia.org/wiki/Guild_of_Saint_Luke'
+        }
+    ]
+};
+
+// Display the function with the education info
+function displayEducation() {
+    for (var school in education.schools) {
+
+        // We loop throught all the schools and create a new div for each school
+        $('#education').append(HTMLschoolStart);
+
+        // Append the info that is not an array
+        $('.education-entry:last').append(HTMLschoolName.replace('%data%', education.schools[school].name) + HTMLschoolDegree.replace('%data%', education.schools[school].degree));
+        $('.education-entry:last').append(HTMLschoolDates.replace('%data%', education.schools[school].dates));
+        $('.education-entry:last').append(HTMLschoolLocation.replace('%data%', education.schools[school].location));
+
+        // Append the info from the 'majors' array, throught a loop
+        if (education.schools[school].majors.length > 0) {
+            var len = education.schools[school].majors.length;
+            for (var major = 0; major < len; major++) {
+                $('.education-entry:last').append(HTMLschoolMajor.replace('%data%', education.schools[school].majors[major]));
+            }
+        }
+
+        // Append the URL in the <a href> from the school name. Also add the target=_blank property to the link.
+        $('.education-entry:last a').attr('href', education.schools[school].url).attr('target', '_blank');
+    }
+}
+displayEducation();
+
+// Create the object 'work'
+var work = {
+    'jobs': [
+        {
+            'employer': 'Palazzo Vecchio',
+            'title': 'Painter',
+            'location': 'Florence, Italy',
+            'dates': ['January 1478'],
+            'description': 'Leonardo was commissioned to paint an altarpiece for the Chapel of St. Bernard in the Palazzo Vecchio, which he never completed.',
+            'url': 'https://en.wikipedia.org/wiki/Palazzo_Vecchio'
+        },
+        {
+            'employer': 'Monks of San Donato',
+            'title': 'Painter',
+            'location': 'San Donato, Italy',
+            'dates': ['March 1481'],
+            'description': 'Leonardo was commissioned to paint <em>The Adoration of the Magi</em> for the monks of San Donato. It was never completed, but we have the uncompleted painting.',
+            'url': 'https://en.wikipedia.org/wiki/Adoration_of_the_Magi_(Leonardo)'
+        },
+        {
+            'employer': 'Ludovico Sforza, Duke of Milan',
+            'title': 'Painter, inventor and engineer',
+            'location': 'Milan, Italy',
+            'dates': ['1482 - 1499'],
+            'description': 'Under the patronage of Ludovico Sforza, he painted the <em>Virgin of the Rocks</em>, <em>The Last Supper</em> and travelled to Hungary to paint another work. He also did engineering projects, like the preparation of floats and pageants for special occasions, and the model for a huge horse bronze monument.',
+            'url': 'https://en.wikipedia.org/wiki/Ludovico_Sforza'
+        },
+        {
+            'employer': 'Matthias Corvinus',
+            'title': 'Painter',
+            'location': 'Buda, Hungary',
+            'dates': ['1485'],
+            'description': 'Painted the <em>Holy Family</em>.',
+            'url': 'https://en.wikipedia.org/wiki/Holy_Family'
+        },
+        {
+            'employer': 'Republic of Venice',
+            'title': 'Military architect and engineer',
+            'location': 'Venice, Italy',
+            'dates': ['1499'],
+            'description': 'He was employed as a military architect and engineer, devising methods to defend the city from naval attacks during the Second Italian War.',
+            'url': 'https://en.wikipedia.org/wiki/Italian_War_of_1499%E2%80%931504'
+        },
+        {
+            'employer': 'Servite monks',
+            'title': 'Painter',
+            'location': 'Florence, Italy',
+            'dates': ['1500'],
+            'description': 'He and his household (his assistant Salai and his mathematician friend Luca Pacioli) were guests at the monastery of Santissima Annuziata. The Servite monks provided Leonardo with a workshop, where he created the drawing <em>The Virgin and Child with St Anne and St John the Baptist</em>.',
+            'url': 'https://en.wikipedia.org/wiki/The_Virgin_and_Child_with_St_Anne_and_St_John_the_Baptist'
+        },
+        {
+            'employer': 'Cesare Borgia',
+            'title': 'Military architect and engineeer',
+            'location': 'Cesena, Italy',
+            'dates': ['1502'],
+            'description': 'Entered the service of Cesare Borgia as a military architect and engineer. Leonardo created a map of Imola and another of Chiana Valley. He also projected the construction of a dam from the sea to Florence, to provide water supply during all the year.',
+            'url': 'https://en.wikipedia.org/wiki/Cesare_Borgia'
+        },
+        {
+            'employer': 'Guild of Saint Luke',
+            'title': 'Painter',
+            'location': 'Florence, Italy',
+            'dates': ['October 1503 - 1504'],
+            'description': 'Rejoined the Guild of Saint Luke and spent two years painting a mural of <em>The Battle of Anghiari</em>, with Michelangelo designing its companion piece, <em>The Battle of Cascina</em>.',
+            'url': 'https://en.wikipedia.org/wiki/The_Battle_of_Anghiari_(painting)'
+        },
+        {
+            'employer': 'Pope Leo X',
+            'title': 'Painter',
+            'location': 'Rome, Italy',
+            'dates': ['September 1513 - 1516'],
+            'description': 'Leonardo spent much of his time living in the Belvedere, in the Vatican, half retired already. After the recapturing of Milan by King Francis I of France, Leonardo was commissioned to make a mechanical lion which could walk forward, open its chest and reveal a cluster of lilies for the king.',
+            'url': 'https://en.wikipedia.org/wiki/Cortile_del_Belvedere'
+        },
+        {
+            'employer': 'King François I of France',
+            'title': 'Inventor',
+            'location': 'Amboise, France',
+            'dates': ['1516 - May 1519'],
+            'description': 'Leonardo went to France to live, where he spend the last years of his life and became a close friend of the king of France, Francis I.',
+            'url': 'https://en.wikipedia.org/wiki/Francis_I_of_France'
+        }
+    ]
+};
+
+// Display the function with the work info
+function displayWork() {
+    for (var job in work.jobs) {
+
+        // We create a new div for each work job
+        $('#workExperience').append(HTMLworkStart);
+
+        // We concat employer and title
+        $('.work-entry:last').append(HTMLworkEmployer.replace('%data%', work.jobs[job].employer) + HTMLworkTitle.replace('%data%', work.jobs[job].title));
+
+        // We append the dates information throught a loop, since it is an array. We have the 'if' to put it online only if there is something in it.
+        if (work.jobs[job].dates.length > 0) {
+            var len = work.jobs[job].dates.length;
+            for (var date = 0; date < len; date++) {
+                $('.work-entry:last').append(HTMLworkDates.replace('%data%', work.jobs[job].dates[date]));
+            }
+        }
+
+        // We append the location and description
+        $('.work-entry:last').append(HTMLworkLocation.replace('%data%', work.jobs[job].location));
+        $('.work-entry:last').append(HTMLworkDescription.replace('%data%', work.jobs[job].description));
+
+        // Append the URL in the <a href> from the work name. Also add the target=_blank property to the link.
+        $('.work-entry:last a').attr('href', work.jobs[job].url).attr('target', '_blank');
+    }
+}
+displayWork();
+
+// Create the object 'projects'
+var projects = {
+    'project': [
+        {
+            'title': 'Paintings of the 1480s',
+            'dates': '1480 - 1489',
+            'description': 'In the 1480s Leonardo recieved three important commissions. <em>St. Jerome in the Wilderness</em>, with a very unusual composition, and the <em>Adoration of the Magi</em>, a commission from the Monks of San Donato a Scopeto, were never finished. Leonardo did numerous drawings and preparatory studies for the second one, but in 1482 he went to Milan and abandoned the painting. The third important work is the <em>Virgin of the Rocks</em>, commissioned for the Confraternity of the Immaculate Conception. This one was done with the assistance of the de Predis brothers, who never got paid for their work...',
+            'images': ['./images/1480-st-jerome.jpg', './images/1481-adoration-magi.jpg', './images/1483-virgin-rocks.jpg'],
+            'url': 'https://en.wikipedia.org/wiki/List_of_works_by_Leonardo_da_Vinci'
+        },
+        {
+            'title': 'Paintings of the 1490s',
+            'dates': '1490 - 1499',
+            'description': '<em>The Last Supper</em> is the most famous painting of this period. It was painted for the refectory of the Convent of Santa Maria della Grazie in Milan, and represents the last meal shared by Jesus with his disciples before his capture and death. The painting was acclaimed as a masterpiece of design and characterisation, but instead of using the technique of fresco, Leonardo used tempera, resulting in a surface subject to mold and flaking and it deteriorated rapidly.',
+            'images': ['./images/1495-virgin-rocks.jpg', './images/1498-last-supper.jpg', './images/1496-belle-ferroniere.jpg'],
+            'url': 'https://en.wikipedia.org/wiki/List_of_works_by_Leonardo_da_Vinci'
+        },
+        {
+            'title': 'Paintings of the 1500s',
+            'dates': '1500 - 1519',
+            'description': 'Among the works created by Leonardo in the 16th century is the small portrait known as the Mona Lisa or <em>la Gioconda</em>, the laughing one. In the present era it is arguably the most famous painting in the world. Its fame rests, in particular, on the elusive smile of the woman.',
+            'images': ['./images/1503-mona-lisa.jpg', './images/1508-john-baptist.jpg', './images/1503-virgin-child.jpg'],
+            'url': 'https://en.wikipedia.org/wiki/List_of_works_by_Leonardo_da_Vinci'
+        },
+        {
+            'title': 'Drawings',
+            'dates': '1470 - 1519',
+            'description': 'Leonardo was not a prolific painter, but he was a most prolific draftsman, keeping journals full of small sketches and detailed drawings recording all manner of things that took his attention. As well as the journals there exist many studies for paintings, some of which can be identified as preparatory to particular works.',
+            'images': ['./images/1490-vitruvian-man.jpg', './images/1500-virgin-child.jpg', './images/1508-head-woman.jpg', './images/old-man.jpg', './images/sedge.jpg'],
+            'url': 'https://en.wikipedia.org/wiki/List_of_works_by_Leonardo_da_Vinci'
+        },
+        {
+            'title': 'Anatomy',
+            'dates': '1470 - 1519',
+            'description': 'His formal training in the anatomy of the human body began with his apprenticeship to Andrea del Verrocchio, who insisted that all his pupils learn anatomy. His anatomical drawings include many studies of the human skeleton and its parts, and studies muscles and sinews. The drawings and notation are far ahead of their time, and if published, would undoubtedly have made a major contribution to medical science. As an artist, Leonardo also closely observed and recorded the effects of age and of human emotion on the physiology, studying in particular the effects of rage. ',
+            'images': ['./images/anatomy1.jpg', './images/anatomy2.jpg', './images/anatomy3.jpg', './images/anatomy4.jpg', './images/anatomy5.jpg', './images/anatomy6.jpg'],
+            'url': 'https://en.wikipedia.org/wiki/Science_and_inventions_of_Leonardo_da_Vinci'
+        },
+        {
+            'title': 'Engineering and inventions',
+            'dates': '1470 - 1519',
+            'description': 'Leonardo was also valued as an engineer. He devised a system of moveable barricades to protect Venice from an attack, he also had a scheme for diverting the flow of the Arno river. He had a vast number of inventions, practical and impractical, in his journals. They include musical instruments, a mechanical knight, hydraulic pumps, reversible crank mechanisms, finned mortar shells, and a steam cannon. For much of his life, he was fascinated by the phenomenon of flight, producing many studies of the flight of birds, as well as plans for several flying machines.',
+            'images': ['./images/flying-machine.jpg', './images/flying-machine2.jpg', './images/cannons.jpg', './images/artillery.jpg'],
+            'url': 'https://en.wikipedia.org/wiki/Science_and_inventions_of_Leonardo_da_Vinci'
+        }
+    ]
+};
+
+// Display the function with the projects
+function displayProjects() {
+    for (var project in projects.project) {
+
+        // We create a new div for each project (painting or invention)
+        $('#projects').append(HTMLprojectStart);
+
+        // We append the title, dates and description
+        $('.project-entry:last').append(HTMLprojectTitle.replace('%data%', projects.project[project].title));
+        $('.project-entry:last').append(HTMLprojectDates.replace('%data%', projects.project[project].dates));
+        $('.project-entry:last').append(HTMLprojectDescription.replace('%data%', projects.project[project].description));
+
+        // We append the project imatges throught a loop, since it is an array. We have the 'if' to put it online only if there is something in it.
+        if (projects.project[project].images.length > 0) {
+            var len = projects.project[project].images.length;
+            for (var image = 0; image < len; image++) {
+                $('.project-entry:last').append(HTMLprojectImage.replace('%data%', projects.project[project].images[image]).replace('%alt%', projects.project[project].title));
+            }
+        }
+
+        // Append the URL in the <a href> from the project name. Also add the target=_blank property to the link.
+        $('.project-entry:last a').attr('href', projects.project[project].url).attr('target', '_blank');
+    }
+}
+displayProjects();
+
+// Maps!
+$('#map-div').append(googleMap);
+
+// jQuery to create the sticky navigation menu
+$(document).ready(function () {
+    // grab the initial top offset of the navigation
+    var stickyNavTop = $('.nav').offset().top;
+
+    // our function that decides weather the navigation bar should have 'fixed' css position or not.
+    var stickyNav = function () {
+        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+        // otherwise change it back to relative
+        if (scrollTop > stickyNavTop) {
+            $('.nav').addClass('sticky');
+        } else {
+            $('.nav').removeClass('sticky');
+        }
+    };
+
+    stickyNav();
+    // and run it again every time you scroll
+    $(window).scroll(function () {
+        stickyNav();
+    });
+});
+
+// jQuery to create a smooth effect when we scrooldown using the menu (the anchor links)
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
